c08919f7314777ddc736ebebc00a11dd1bff3270
Mike Grudic <mgrudich@caltech.edu>
2018-11-28 13:52:49 -0800

    option to compute force/potential with a pre-generated tree

--- pykdgrav/__init__.py
+++ pykdgrav/__init__.py
@@ -2,7 +2,7 @@ import numpy as np
 from .kdtree import *
 from .treewalk import *
 
-def Potential(x, m, softening=None, G=1., theta=1., parallel=False):
+def Potential(x, m, softening=None, G=1., theta=1., parallel=False, tree=None):
     """Returns the approximate gravitational potential for a set of particles with positions x and masses m.
 
     Arguments:
@@ -13,17 +13,18 @@ def Potential(x, m, softening=None, G=1., theta=1., parallel=False):
     G -- gravitational constant (default 1.0)
     theta -- cell opening angle used to control force accuracy; smaller is faster but more accurate. (default 1.0, gives ~1% accuracy)
     parallel -- If True, will parallelize the force summation over all available cores. (default False)
+    tree -- optional pre-generated kd-tree (default None)
     """
     if softening is None:
         softening = np.zeros_like(m)
-    tree = ConstructKDTree(np.float64(x),np.float64(m), np.float64(softening))
+    if not tree: tree = ConstructKDTree(np.float64(x),np.float64(m), np.float64(softening))
     result = zeros(len(m))
     if parallel:
         return GetPotentialParallel(np.float64(x),tree,G,theta)
     else:
         return GetPotential(np.float64(x),tree,G,theta)
 
-def Accel(x, m, softening=None, G=1., theta=1., parallel=False):
+def Accel(x, m, softening=None, G=1., theta=1., parallel=False, tree=None):
     """Returns the approximate gravitational acceleration for a set of particles with positions x and masses m.
 
     Arguments:
@@ -33,20 +34,22 @@ def Accel(x, m, softening=None, G=1., theta=1., parallel=False):
     Keyword arguments:
     G -- gravitational constant (default 1.0)
     theta -- cell opening angle used to control force accuracy; smaller is faster but more accurate. (default 1.0, gives ~1% accuracy)
-    parallel -- If True, will parallelize the force summation over all available cores. (default False) """
+    parallel -- If True, will parallelize the force summation over all available cores. (default False) 
+    tree -- optional pre-generated kd-tree (default None)
+"""
     
     if softening is None:
         softening = np.zeros_like(m)
-    tree = ConstructKDTree(np.float64(x),np.float64(m), np.float64(softening))
+    if not tree = ConstructKDTree(np.float64(x),np.float64(m), np.float64(softening))
     result = zeros_like(x)
     if parallel:
         return GetAccelParallel(np.float64(x), tree, G, theta)
     else:
         return GetAccel(np.float64(x), tree, G, theta)
 
-def CorrelationFunction(x, m, rbins, frac=1.):
+def CorrelationFunction(x, m, rbins, frac=1., tree=None):
     N = len(x)
-    tree = ConstructKDTree(np.float64(x), np.float64(m))
+    if not tree = ConstructKDTree(np.float64(x), np.float64(m))
     counts = zeros(len(rbins)-1, dtype=np.int64)
     for i in range(N):
         if np.random.rand() < frac:
