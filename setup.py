0d9139f1feb990442155b865c6ac5b6e413c5755
Mike Grudic <mike.grudich@gmail.com>
2021-06-11 13:37:24 -0700

    small fixes to frontend revision, readme notebook

--- setup.py
+++ setup.py
@@ -1,16 +1,3 @@
-# from setuptools import setup
-
-# setup(name='pytreegrav',
-#       version='0.1',
-#       description='Fast approximate gravitational force and potential calculations',
-#       url='http://github.com/mikegrudic/pytreegrav',
-#       author='Mike Grudic',
-#       author_email='mike.grudich@gmail.com',
-#       license='MIT',
-#       packages=['pytreegrav'],
-#       zip_safe=False)
-
-
 import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fh:
@@ -18,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pytreegrav",
-    version="0.1",
+    version="0.2",
     author="Mike Grudic",
     author_email="mike.grudich@gmail.com",
     description='Fast approximate gravitational force and potential calculations',
--- src/pytreegrav/frontend.py
+++ src/pytreegrav/frontend.py
@@ -35,6 +35,9 @@ def Potential(pos, m, softening=None, G=1., theta=.7, tree=None, return_tree=Fal
     tree -- optional pre-generated Octree: this can contain any set of particles, not necessarily the target particles at pos (default None)
     return_tree -- return the tree used for future use (default False)
     method -- 'adaptive', 'tree', or 'bruteforce' (default adaptive tries to pick the faster choice)
+
+    Returns:
+    phi -- shape (N,) array of potentials at the particle positions
     """
     if softening is None: softening = np.zeros_like(m)
 
@@ -58,7 +61,56 @@ def Potential(pos, m, softening=None, G=1., theta=.7, tree=None, return_tree=Fal
             phi = PotentialTarget_tree(pos,softening,tree,theta=theta,G=G)
 
     if return_tree:
-        return tree, phi
+        return phi, tree
+    else:
+        return phi
+
+def PotentialTarget(pos_target, pos_source, m_source, h_target=None, h_source=None, G=1., theta=.7, tree=None, return_tree=False,parallel=False,method='adaptive'):
+    """Returns the gravitational potential due to a set of particles, at a set of "target" positions that can be different from the particle positions, using either brute force or tree-based methods depending on the number of particles and target positions.
+
+    Arguments:
+    pos_target -- shape (N,3) array of target positions where the potential is to be computed
+    pos_source -- shape (M,3) array of source positions of the particles generating the potential
+    m_source -- shape (M,) array of particle masses    
+    
+
+    Optional arguments:
+    h_target -- shape (N,) array of target softening radii - this will be the _minimum_ softening length used in any interaction computed for this target point
+    h_source -- shape (M,) array of source softening radii
+    G -- gravitational constant (default 1.0)
+    theta -- cell opening angle used to control force accuracy; smaller is slower (runtime ~ theta^-3) but more accurate. (default 0.7, gives ~1% accuracy)
+    parallel -- If True, will parallelize the force summation over all available cores. (default False)
+    tree -- optional pre-generated Octree: this can contain any set of particles, not necessarily the target particles at pos (default None)
+    return_tree -- return the tree used for future use (default False)
+    method -- 'adaptive', 'tree', or 'bruteforce' (default adaptive tries to pick the faster choice)
+
+    Returns:
+    phi -- shape (N,) array of potentials at the target positions
+    """
+    if h_target is None: h_target = np.zeros(len(pos_target))
+    if h_source is None: h_source = np.zeros(len(pos_source))
+
+    # figure out which method to use
+    if method == 'adaptive':
+        if len(pos_target)*len(pos_source) > 10**6: method = 'tree'
+        else: method = 'bruteforce'
+
+    if method == 'bruteforce': # we're using brute force
+        if parallel:
+            phi = PotentialTarget_bruteforce_parallel(pos_target,h_target,pos_source,m_source,h_source,G=G)
+        else:
+            phi = PotentialTarget_bruteforce(pos_target,h_target,pos_source,m_source,h_source,G=G)
+        if return_tree:
+            tree = None
+    else: # we're using the tree algorithm
+        if tree is None: tree = ConstructTree(np.float64(pos_source),np.float64(m_source), np.float64(h_source)) # build the tree if needed
+        if parallel:
+            phi = PotentialTarget_tree_parallel(pos_target,h_target,tree,theta=theta,G=G)
+        else:
+            phi = PotentialTarget_tree(pos_target,h_target,tree,theta=theta,G=G)
+
+    if return_tree:
+        return phi, tree
     else:
         return phi
             
@@ -101,6 +153,56 @@ def Accel(pos, m, softening=None, G=1., theta=.7, tree=None, return_tree=False,p
             g = AccelTarget_tree(pos,softening,tree,theta=theta,G=G)
 
     if return_tree:
-        return tree, g
+        return g, tree
+    else:
+        return g
+
+def AccelTarget(pos_target, pos_source, m_source, h_target=None, h_source=None, G=1., theta=.7, tree=None, return_tree=False, parallel=False, method='adaptive'):
+    """Returns the gravitational acceleration due to a set of particles, at a set of "target" positions that can be different from the particle positions, using either brute force or tree-based methods depending on the number of particles and target positions.
+
+    Arguments:
+    pos_target -- shape (N,3) array of target positions where the field is to be computed
+    pos_source -- shape (M,3) array of source positions of the particles generating the field
+    m_source -- shape (M,) array of particle masses    
+    
+
+    Optional arguments:
+    h_target -- shape (N,) array of target softening radii - this will be the _minimum_ softening length used in any interaction computed for this target point
+    h_source -- shape (M,) array of source softening radii
+    G -- gravitational constant (default 1.0)
+    softening -- shape (N,) array containing kernel support radii for gravitational softening - set to 0 by default
+    theta -- cell opening angle used to control force accuracy; smaller is slower (runtime ~ theta^-3) but more accurate. (default 0.7, gives ~1% accuracy)
+    parallel -- If True, will parallelize the force summation over all available cores. (default False)
+    tree -- optional pre-generated Octree: this can contain any set of particles, not necessarily the target particles at pos (default None)
+    return_tree -- return the tree used for future use (default False)
+    method -- 'adaptive', 'tree', or 'bruteforce' (default adaptive tries to pick the faster choice)
+
+    Returns:
+    g -- shape (N,3) array of gravitational fields at the target positions
+    """
+    if h_target is None: h_target = np.zeros(len(pos_target))
+    if h_source is None: h_source = np.zeros(len(pos_source))
+
+    # figure out which method to use
+    if method == 'adaptive':
+        if len(pos_target)*len(pos_source) > 10**6: method = 'tree'
+        else: method = 'bruteforce'
+
+    if method == 'bruteforce': # we're using brute force
+        if parallel:
+            g = AccelTarget_bruteforce_parallel(pos_target,h_target,pos_source,m_source,h_source,G=G)
+        else:
+            g = AccelTarget_bruteforce(pos_target,h_target,pos_source,m_source,h_source,G=G)
+        if return_tree:
+            tree = None
+    else: # we're using the tree algorithm
+        if tree is None: tree = ConstructTree(np.float64(pos_source),np.float64(m_source), np.float64(h_source)) # build the tree if needed
+        if parallel:
+            g = AccelTarget_tree_parallel(pos_target,h_target,tree,theta=theta,G=G)
+        else:
+            g = AccelTarget_tree(pos_target,h_target,tree,theta=theta,G=G)
+
+    if return_tree:
+        return g, tree
     else:
         return g
