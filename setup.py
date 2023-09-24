6d05f8862eabb0b0d56ffc932a95778b1ccfcc8f
Mike Grudic <mike.grudich@gmail.com>
2023-09-24 21:19:01 -0230

    version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if os.path.isfile(requirementPath):
 
 setuptools.setup(
     name="pytreegrav",
-    version="1.1",
+    version="1.1.1",
     author="Mike Grudic",
     author_email="mike.grudich@gmail.com",
     description="Fast approximate gravitational force and potential calculations",
